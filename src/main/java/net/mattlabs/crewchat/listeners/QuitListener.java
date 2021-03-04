4e6d5d121484b39c45d842487dfae8bd9ef172ac
mattboy9921 <mattboy9921@aim.com>
2021-03-04 12:25:19 -0500

    Undeafen before setting offline

--- src/main/java/net/mattlabs/crewchat/listeners/QuitListener.java
+++ src/main/java/net/mattlabs/crewchat/listeners/QuitListener.java
@@ -24,7 +24,7 @@ public class QuitListener implements Listener {
 
         Player player = event.getPlayer();
         msgManager.removePlayer(player.getDisplayName());
-        if (playerManager.isOnline(player)) playerManager.setOffline(player);
         if (playerManager.isDeafened(player)) playerManager.setDeafened(player, false);
+        if (playerManager.isOnline(player)) playerManager.setOffline(player);
     }
 }
